d0f493f67a7b36d14f9635530561d3a788907780
Sara Carreras <saracc621@gmail.com>
2024-03-21 20:10:09 +0100

    add socket dependency

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user has connected!");
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected!");
+  });
+});
+
 app.use(logger("dev"));
 app.use(express.static(process.cwd() + "/src/client"));
 
@@ -11,6 +24,6 @@ app.get("/", (_req, res) => {
   res.sendFile(process.cwd() + "/src/client/index.html");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
